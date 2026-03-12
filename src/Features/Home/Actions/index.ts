2a05323a99b47121bc13b68c615e5ef2cbcf47db
Criss.dev <138832591+criss-dev-01@users.noreply.github.com>
2026-03-12 12:49:24 -0500

    cambios con la integracion del backend

--- src/Features/Home/Actions/index.ts
+++ src/Features/Home/Actions/index.ts
@@ -1,44 +1,80 @@
 'use server'
 import { cookies } from 'next/headers'
 
-import { ContentResponse } from '@/Features/Home/Types/Content.types'
+import { ContentFetchResponse, ContentResponse, ContentToPlay } from '@/Features/Home/Types/Content.types'
 
 export const signOutUser = async (): Promise<void> => {
   const cookieStore = await cookies()
   cookieStore.delete('auth_token')
 }
 
+const prepareTypeContent = (url: string): 'video' | 'image' => {
+  const extensionValid = ['gif', 'jpg', 'png', 'jpeg', 'mp4']
+  const type = url.split('.').pop() || ''
+  if (extensionValid.includes(type)) {
+    if (type === 'mp4') {
+      return 'video'
+    } else {
+      return 'image'
+    }
+  } else {
+    throw new Error('Invalid file type')
+  }
+}
+
+const prepareDataContent = (data: ContentFetchResponse): ContentResponse => {
+  const contentToPlay: ContentToPlay[] = data.data.playlists.flatMap((playlist) =>
+    playlist.media_files.map((media) => ({
+      id: media.id,
+      url: `${process.env.S3_BUCKET_URL}/${media.path}`,
+      type: prepareTypeContent(media.path),
+      duration: media.duration
+    }))
+  )
+
+  return {
+    status: 'success',
+    message: 'Content fetched successfully',
+    data: contentToPlay
+  }
+}
+
 export const getDataContent = async (): Promise<ContentResponse> => {
   const cookieStore = await cookies()
   try {
-    await new Promise((resolve) => setTimeout(resolve, 3000))
     const authToken = cookieStore.get('auth_token')?.value || ''
 
-    return {
-      status: 'success',
-      message: `Content fetched successfully ${authToken}`,
-      data: [
-        {
-          id: 1,
-          url: 'https://btbox-2023.s3.amazonaws.com/236711_small.mp4',
-          type: 'video'
-        },
-        {
-          id: 2,
-          url: 'https://btbox-2023.s3.amazonaws.com/293788_small.mp4',
-          type: 'video'
-        },
-        {
-          id: 3,
-          url: 'https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_656/b_white/f_auto/q_auto/store/software/switch/70070000030771/348449056f8d3665c946218d0fd76d80431afaa272a9984fc05bed46e895379c',
-          type: 'image'
-        },
-        {
-          id: 5,
-          url: 'https://btbox-2023.s3.amazonaws.com/WhatsApp%20Video%202025-11-13%20at%2014.55.09.mp4',
-          type: 'video'
+    const path = new URL(`${process.env.BACKEND_URL}/playlists`)
+    path.searchParams.append('key', authToken)
+    const data = await fetch(path.toString(), {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': process.env.CONNECT_KEY || ''
+      },
+      cache: 'force-cache',
+      next: { revalidate: parseInt(process.env.NEXT_PUBLIC_TIMEOUT_FETCH || '7000') }
+    })
+
+    if (data.status !== 200) {
+      if (data.status === 404) {
+        await signOutUser()
+        return {
+          status: 'error',
+          message: 'Invalid code',
+          data: null
         }
-      ]
+      } else {
+        return {
+          status: 'error',
+          message: 'An error occurred during authentication',
+          data: null
+        }
+      }
+    } else {
+      const playList: ContentFetchResponse = await data.json()
+      const response = prepareDataContent(playList)
+      return response
     }
   } catch (error) {
     console.error('Error during authentication:', error)
--- src/Features/Home/Types/Content.types.ts
+++ src/Features/Home/Types/Content.types.ts
@@ -4,6 +4,42 @@ export interface ContentToPlay {
   id: number
   url: string
   type: 'video' | 'image'
+  duration: number | null
 }
 
 export type ContentResponse = ActionResponse<ContentToPlay[] | null>
+
+export interface ContentPlayer {
+  id: number
+  key: string
+  name: string
+}
+
+export interface Playlist {
+  id: number
+  name: string
+  start_date: Date
+  end_date: Date
+  start_time: string
+  end_time: string
+  priority: number
+  media_files: MediaFile[]
+}
+
+export interface MediaFile {
+  id: number
+  name: string
+  path: string
+  orientation: string
+  start_date: Date
+  end_date: Date
+  duration: number | null
+}
+
+export interface ContentFetchResponse {
+  status: 'success' | 'error'
+  data: {
+    content_player: ContentPlayer
+    playlists: Playlist[]
+  }
+}
